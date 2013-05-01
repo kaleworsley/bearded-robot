dcd61938f43744fbae0a912fd3353dd52afaf829
Kale Worsley <kale@worsley.co.nz>
2013-05-01 20:36:03 +1200

    Initial commit.
    :)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeardedRobot::Application.config.secret_key_base = 'c2188772f3a49bd570fdf545e88ed5495b9ef3cbc957887c3ca46c626e37449dcaad2ed87233e5b54741b20e86412db4328c8035e03c3a49a182a98ebcb1e7ef'
