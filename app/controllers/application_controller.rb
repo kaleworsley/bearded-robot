f6ed074ff10b403ddedadec57e3e9c6e2e142b00
Kale Worsley <kale@worsley.co.nz>
2013-06-10 00:02:25 +1200

    Rescue from AuthorizationSystem::NotAuthorized exceptions.
    [:musical_note: On the Other Side - Justin Perkins]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from 'AuthorizationSystem::NotAuthorized' do |exception|
+    if current_user
+      redirect_to :root, :alert => 'Not Authorized'
+    else
+      redirect_to login_path, :alert => 'Not Authorized'
+    end
+  end
+
   private
 
   def current_user
